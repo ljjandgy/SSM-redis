56f71be09adde588aedf806a8d927d179d7ffcc9
ljj <410439770@qq.com>
2018-11-18 23:02:37 +0800

    拦截器测试类

---
+++ src/test/java/com/example/springredis/InterceptorTest.java
@@ -0,0 +1,14 @@
+package com.example.springredis;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 拦截器测试
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InterceptorTest {
+
+}
