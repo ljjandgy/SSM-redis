cb805a9eb5b690a0d47b9a632680448797234930
luojunjie <luojunjie@inspur.com>
2018-12-12 21:27:00 +0800

    redis操作

---
+++ src/main/java/com/example/springredis/redis/RedisListTest.java
@@ -0,0 +1,10 @@
+package com.example.springredis.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class RedisListTest {
+    @Autowired
+    RedisTemplate redisTemplate;
+
+}
