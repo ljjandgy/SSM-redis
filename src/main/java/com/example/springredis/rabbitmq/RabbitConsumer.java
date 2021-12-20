2eab114caacfd74db3af749d87bd2697d27813bf
luojunjie <luojunjie9494@163.com>
2021-12-20 23:25:25 +0800

    新增mq消费代码

---
+++ src/main/java/com/example/springredis/rabbitmq/RabbitConsumer.java
@@ -0,0 +1,43 @@
+package com.example.springredis.rabbitmq;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class RabbitConsumer {
+    private static final String QUEUE_NAME = "queue_demo";
+
+    private static final String IP_ADDRESS = "192.168.92.129";
+
+    private static final int PORT = 5672;
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        Address[] addresses = new Address[]{
+                new Address(IP_ADDRESS,PORT)
+        };
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("root");
+        factory.setPassword("123456");
+        Connection connection = factory.newConnection(addresses);
+        final Channel channel = connection.createChannel();
+        //设置客户端最多接受未被ack的消息个数
+        Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("receive message " + new String(body));
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                channel.basicAck(envelope.getDeliveryTag(),false);
+            }
+        };
+        channel.basicConsume(QUEUE_NAME,consumer);
+        TimeUnit.SECONDS.sleep(5);
+        channel.close();
+        connection.close();
+    }
+}
